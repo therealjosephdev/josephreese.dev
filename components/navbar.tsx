ae65e53c3ffcf54652e43ff7d36ea1a800b080c1
Joseph Reese <therealjosephhh@gmail.com>
2024-08-09 09:51:38 +0300

    Removed Logo

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,15 +9,7 @@ export function NavBar() {
       <div className="grid gap-6">
         <div className="flex items-center justify-between">
           <Link href="/" className="flex items-end gap-2">
-            {/* <Image
-              priority
-              className="invert transition-all hover:opacity-70 dark:invert-0"
-              src={logo}
-              width={54}
-              height={43.97}
-              alt="Joseph Reese logo"
-            ></Image> */}
-            <p className="hidden md:block">Joseph Reese</p>
+            <p>Joseph Reese</p>
           </Link>
           <div className="flex items-center gap-4 text-base">
             <a
